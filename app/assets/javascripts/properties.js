44536a278b78c90c03ec1eb13b28e71287df9b93
Rafael Montas <rafaelmontas1@gmail.com>
2017-03-12 16:09:56 -0400

    Init

---
+++ app/assets/javascripts/properties.js
@@ -0,0 +1,79 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// Nav
+$(document).on('turbolinks:load', function() {
+  $(".navigation-trigger, .navigation-close").click(function() {
+    $(".navigation").toggleClass("toggled");
+    $('body').toggleClass("nav-opened");
+  });
+  $(".btn-action, .btn-link").click(function() {
+    $(".sidebar-mobile").toggleClass("toggled");
+    $("body").toggleClass("block-opened");
+  });
+
+  $(".rent_or_buy").click(function() {
+    $("div.search-type").removeClass("open")
+  });
+  // if($('.form-control').val().length === 0){
+  //   $('.form-group').toggleClass("form-group-active");
+  //  }
+
+  $(".nav-drop").hover(function() {
+    $(".nav-drop-menu", this).css("display", "block");
+  }, function() {
+    $(".nav-drop-menu", this).css("display", "none");
+  });
+
+  $(".search-body .search-input").focusin(function() {
+    $(".search-body").addClass("toggled");
+  });
+  // $(".search-body, .search-advanced").focusout(function() {
+  //   $(".search-body").toggleClass("toggled");
+  // });
+  $(".prevent").click(function() {
+    event.preventDefault();
+  });
+  $(".search-advanced .cancel").click(function() {
+    $(".search-body").removeClass("toggled");
+  });
+
+
+  // slick carousel
+  $('.listings-grid').slick({
+    arrows: true,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+});
