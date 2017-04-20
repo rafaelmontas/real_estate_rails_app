75ab1438db5f56e5ac50fab1a0bb39869397d663
Rafael Montas <rafaelmontas1@gmail.com>
2017-04-19 20:45:53 -0400

    Add User model, validations, and tests...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412180205) do
+ActiveRecord::Schema.define(version: 20170420002441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,14 @@ ActiveRecord::Schema.define(version: 20170412180205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "image_data"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
